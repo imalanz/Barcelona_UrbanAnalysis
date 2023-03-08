010e10d67f08a9f7dc9e764fb52d5e48f1570140
imalanz <arq.imalanz@gmail.com>
2023-03-08 17:57:03 +0100

    mier

--- src/inmobi.py
+++ src/inmobi.py
@@ -106,3 +106,42 @@ def circulacion (url):
                             5:"4.Les Corts", 6:"5.Sarrià-Sant Gervasi", 7:"6.Gràcia", 8:"7.Horta-Guiardó",
                             9: "8.Nou Barris", 10:"9.Sant Andreu", 11:"10.Sant Martí"}, inplace=True)
     return df
+
+# scraping for incidents with mossos by district.
+def mossos_inc (url):   
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    table = soup.find_all("table")[0]
+    tags = table.find_all("tr")
+    # select only texts and clean it.
+    rows = [i.text.strip().replace("\xa0", "").split("\n") for i in tags]
+    x = rows[9:-6]
+    df = pd.DataFrame(x)
+    # clean and rename.
+    df.drop(columns=[12], inplace=True)
+    df.rename(columns={0: "Incidencias", 1:"Barcelona", 2:"1.Ciutat Vella", 3:"2.L'Eixample", 4:"3.Sants-Monjuïc",
+                                5:"4.Les Corts", 6:"5.Sarrià-Sant Gervasi", 7:"6.Gràcia", 8:"7.Horta-Guiardó",
+                                9: "8.Nou Barris", 10:"9.Sant Andreu", 11:"10.Sant Martí"}, inplace=True)
+    return df 
+
+# poblation census per years old.
+def poblation (url): 
+    html = requests.get(url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    table = soup.find_all("table")[0]
+    tags = table.find_all("tr")
+    # select only texts and clean it.
+    rows = [i.text.strip().replace("\xa0", "").split("\n") for i in tags]
+    x = rows[9:-6]
+    colum = rows[4]
+    # transform to df.
+    df = pd.DataFrame(x, columns=colum)
+    # Create new columns with clean info.
+    df["Barrio"] = df["Dto.Barrios"].apply(lambda x: x.split(".")[1])
+    df["Dto."] = df["Dto.Barrios"].apply(lambda x: x.split(".")[0])
+    # Separate mixed nummbers to district numb.
+    df["Dto."] = df["Dto."].apply(nums)
+    # clean first column names, change the names of columns.
+    df.drop(columns=["Dto.Barrios"], inplace=True)
+    df = df[["Dto.", "Barrio", "TOTAL", "0-14 años", "15-24 años", "25-64 años", "65 años y más"]]
+    return df
