b607286d5501386d5923a6d0a92a5b241408e285
imalanz <arq.imalanz@gmail.com>
2023-03-14 00:04:23 +0100

    monda

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
+st.title("Barcelona Urban Analysis")
+
+tabs
+
 st.set_page_config(page_title="Barcelona Healthy Barri", 
             layout = "wide",
             initial_sidebar_state="expanded")
